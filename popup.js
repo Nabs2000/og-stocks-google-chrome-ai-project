c684171a934d8789ebb943569f8904377daa3305
rafad900 <19312533+rafad900@users.noreply.github.com>
2025-10-15 08:28:33 -0700

    Initial extension

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('alertButton');
+    
+    button.addEventListener('click', () => {
+        alert('Hello from your Chrome Extension!');
+
+        // Example of using a Chrome API to get the current tab's URL
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log('Current URL is: ' + tabs[0].url);
+        });
+    });
+});
