ddda9b19f7b3dc37dc7f06fa86bc2a2c598d7754
rafad900 <19312533+rafad900@users.noreply.github.com>
2025-10-27 17:44:02 -0700

    added the popup back

--- background.js
+++ background.js
@@ -2,4 +2,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "TEXT_SELECTED") {
         sendResponse({ status: "ok" });
     }
+});
+
+chrome.action.onClicked.addListener(function() {
+    chorme.tabs.create({ url: 'popup.html' })
 })
